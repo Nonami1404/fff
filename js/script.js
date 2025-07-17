62657ab422c5ed760a662a8b2691f4ed5af2bfe7
Nonami <saidazam140409@gmail.com>
2025-07-17 21:21:47 +0500

    Final product

--- js/script.js
+++ js/script.js
@@ -332,5 +332,22 @@ window.addEventListener("DOMContentLoaded", () => {
   }
 });
 
+new Swiper('.swiper', {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+    1200: {
+      slidesPerView: 3,
+    }
+  }
+});
+
 
 });
